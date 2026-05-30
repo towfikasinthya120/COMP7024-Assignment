9150e900b190b106ce5e4c5ef33f97b7b8e723bb
towfikasinthya120 <towfikasinthya120@gmail.com>
2026-05-30 21:58:29 +1000

    Converting signup date for Q1

--- Q1.R
+++ Q1.R
@@ -1,9 +1,17 @@
 library(dplyr)
 library(ggplot2)
 library(knitr)
-
+#loading datasets
 players <- read.csv("players.csv")
 sessions <- read.csv("sessions.csv")
 
+
 head(players)
 head(sessions)
+
+#Converting signup date
+players$signup_date <- as.Date(players$signup_date)
+
+str(players)
+summary(players$signup_date)
+
