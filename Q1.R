b48a4b532e33c4fb4379384cb105fa961757eafe
towfikasinthya120 <towfikasinthya120@gmail.com>
2026-05-30 22:57:42 +1000

    Calculated Q1 engagement statistics

--- Q1.R
+++ Q1.R
@@ -29,3 +29,22 @@ players$experience_level[players$signup_date > as.Date("2023-12-31")] <- "New"
 
 table(players$experience_level)
 head(players)
+
+#Merging sessions with players
+q1_data <- left_join(sessions, players, by = "player_id")
+
+head(q1_data)
+nrow(q1_data)
+
+#creating the summary table
+q1_summary <- q1_data %>%
+  group_by(experience_level) %>%
+  summarise(
+    unique_players = n_distinct(player_id),
+    total_play_time = sum(play_time_minutes),
+    avg_play_time_session = mean(play_time_minutes),
+    avg_play_time_player = total_play_time / unique_players,
+    avg_score = mean(score)
+  )
+
+q1_summary
