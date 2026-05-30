e5097704880b3d7c46f7f1c12981c6901bf448a2
towfikasinthya120 <towfikasinthya120@gmail.com>
2026-05-30 23:29:12 +1000

    Calculated Q2 genre statistics

--- Q2.R
+++ Q2.R
@@ -1,14 +1,12 @@
-# Q2: Genre-Level Behaviour and Efficiency
-
 library(dplyr)
 library(ggplot2)
 library(knitr)
 
-# Load required datasets
+# Loading required datasets
 games <- read.csv("games.csv")
 sessions <- read.csv("sessions.csv")
 
-# Check the datasets
+# Checking the datasets
 head(games)
 head(sessions)
 
@@ -17,3 +15,17 @@ q2_data <- left_join(sessions, games, by = "game_id")
 
 # Check merged data
 head(q2_data)
+
+# Calculate genre-level behaviour statistics
+q2_summary <- q2_data %>%
+  group_by(genre) %>%
+  summarise(
+    total_play_time = sum(play_time_minutes),
+    number_of_sessions = n(),
+    unique_players = n_distinct(player_id),
+    avg_play_time_per_player = total_play_time / unique_players,
+    score_efficiency = sum(score) / sum(play_time_minutes)
+  )
+
+# View results
+q2_summary
