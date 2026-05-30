c99791b816c81caf4f1255db52b01faf76486ace
towfikasinthya120 <towfikasinthya120@gmail.com>
2026-05-30 22:18:49 +1000

    Creating Q1 player experience groups

--- Q1.R
+++ Q1.R
@@ -15,3 +15,17 @@ players$signup_date <- as.Date(players$signup_date)
 str(players)
 summary(players$signup_date)
 
+#Creating experience groups
+players$experience_level <- NA
+
+players$experience_level[players$signup_date < as.Date("2023-01-01")] <- "Veteran"
+
+players$experience_level[
+  players$signup_date >= as.Date("2023-01-01") &
+    players$signup_date <= as.Date("2023-12-31")
+] <- "Intermediate"
+
+players$experience_level[players$signup_date > as.Date("2023-12-31")] <- "New"
+
+table(players$experience_level)
+head(players)
