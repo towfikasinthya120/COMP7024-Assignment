7c6de1447dab8b60ed4d636b6dd1abfe608e3274
towfikasinthya120 <towfikasinthya120@gmail.com>
2026-05-31 15:11:16 +1000

    Calculated player level statistics for Q3

--- Q3.R
+++ Q3.R
@@ -9,3 +9,15 @@ sessions <- read.csv("sessions.csv")
 head(players)
 head(sessions)
 
+#create Player Statistics
+player_stats <- sessions %>%
+  group_by(player_id) %>%
+  summarise(
+    total_sessions = n(),
+    total_play_time = sum(play_time_minutes),
+    avg_play_time = mean(play_time_minutes),
+    avg_score = mean(score),
+    unique_games_played = n_distinct(game_id)
+  )
+
+head(player_stats)
